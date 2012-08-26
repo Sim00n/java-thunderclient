dd42b46081a987042591b11a4099ba86dcae7d91
Sim00n <portalowicz@gmail.com>
2012-08-26 01:57:39 -0400

    Removed unnecessary code from testing phase.

--- Tester.java
+++ Tester.java
@@ -1,11 +1,8 @@
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-import org.json.simple.parser.ParseException;
 
 public class Tester {
 
-	public static void main(String[] args) throws IOException, ParseException, NoSuchAlgorithmException {
+	public static void main(String[] args) throws IOException {
 		
 		Thunder thunder = new Thunder("localhost", 8080, "key", "secret");		
 		System.out.println(thunder.getUserInChannel("default-channel"));
